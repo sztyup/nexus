0b064d014d1c35b2810dad8e4775bd48fa9afb25
Sztyup <sztyup@gmail.com>
2017-12-07 01:33:29 +0100

    ServiceProvider cleanup

--- src/Multisite/MultisiteServiceProvider.php
+++ src/Multisite/MultisiteServiceProvider.php
@@ -4,15 +4,37 @@ namespace Sztyup\Multisite;
 
 use Illuminate\Foundation\Application;
 use Illuminate\Support\ServiceProvider;
+use Sztyup\Multisite\Commands\InitializeCommand;
 use Sztyup\Multisite\Middleware\StartSession;
 
 class MultisiteServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/path/to/config/courier.php' => config_path('courier.php'),
+        ], 'config');
+
+        $this->loadRoutesFrom(__DIR__.'/../routes/auth.php');
+
+        $this->loadViewsFrom(__DIR__.'/../view', 'multisite');
+
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                InitializeCommand::class,
+            ]);
+        }
+    }
+
+    protected function bladeDirectives()
+    {
         // @route blade funcion, for site specific routes
         \Blade::directive("route", function($expression) {
             return "<?php echo site()->route($expression); ?>";
+        });
+
+        \Blade::directive("resource", function() {
+            return "<?php echo  ?>";
         });
 
         \Blade::directive("internal_auth", function($expression) {
@@ -27,12 +49,17 @@ class MultisiteServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->app->singleton('site.manager', function (Application $app) {
+        $this->app->singleton('multisite', function (Application $app) {
             return new SiteManager($app);
         });
 
-        $this->app->alias('site.manager', SiteManager::class);
+        $this->app->alias('multisite', SiteManager::class);
 
         $this->app->singleton(StartSession::class);
+
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/multisite.php', 'courier'
+        );
+
     }
 }
