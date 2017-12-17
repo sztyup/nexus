cf5e1a122f6bd9a276588119b8536fdc5af845f6
Sztyup <sztyup@gmail.com>
2017-12-17 17:40:03 +0100

    Middleware for injecting images

---
+++ src/Nexus/Middleware/InjectCrossDomainLogin.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Sztyup\Nexus\Middleware;
+
+use Illuminate\Contracts\Auth\Guard;
+use Illuminate\Contracts\Encryption\Encrypter;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Sztyup\Nexus\SiteManager;
+use Closure;
+
+class InjectCrossDomainLogin
+{
+    protected $siteManager;
+
+    protected $viewFactory;
+
+    protected $guard;
+
+    protected $encrypter;
+
+    public function __construct(SiteManager $siteManager, Factory $viewFactory, Guard $guard, Encrypter $encrypter)
+    {
+        $this->siteManager = $siteManager;
+        $this->viewFactory = $viewFactory;
+        $this->guard = $guard;
+        $this->encrypter = $encrypter;
+    }
+
+    public function handle(Request $request, Closure $next)
+    {
+        /** @var Response $response */
+        $response = $next($request);
+
+        // Only inject if authenticated
+        if($this->guard->guest()) {
+            return $response;
+        }
+
+        // Only inject html responses
+        if(!Str::contains($response->headers->get('Content-Type'), 'html')) {
+            return $response;
+        }
+
+        // Inject images before the closing body tag
+        $response->setContent(
+            Str::replaceFirst("</body>", $this->getInjectedCode($request->getSession()) . "</body>", $response->getContent())
+        );
+
+        return $response;
+    }
+
+    protected function getInjectedCode(SessionInterface $session): string
+    {
+        return $this->viewFactory->make('nexus:cdimages', [
+            'sites' => $this->siteManager->all(),
+            'code' => $this->encrypter->encrypt($session->getId())
+        ])->render();
+    }
+}
