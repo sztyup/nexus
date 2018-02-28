cdca941aef9ba75de96d76afddad1d7808090bce
Sztyup <sztyup@gmail.com>
2018-02-28 12:36:53 +0100

    Append query string manually

--- src/view/cdimages.blade.php
+++ src/view/cdimages.blade.php
@@ -1,6 +1,6 @@
 {{-- DO NOT OVERRIDE OR EDIT THIS VIEW --}}
 <!-- Cross domain login handling -->
 @foreach($sites as $site)
-    <img src="{{ $site->route('auth.internal', ['s_code' => $code]) }}" class="nothing"/>
+    <img src="{{ $site->route('auth.internal') }}?s_code={{ $code }}" class="nothing"/>
 @endforeach
 <!-- End of cross domain login handling -->
