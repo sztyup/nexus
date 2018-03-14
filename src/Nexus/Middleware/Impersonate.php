3c4e77a7e5ade4ce498fade55d877b3b01b4a105
sztyup <sztyup@gmail.com>
2018-03-14 19:39:03 +0100

    Middleware refactored to use manager

--- src/Nexus/Middleware/Impersonate.php
+++ src/Nexus/Middleware/Impersonate.php
@@ -3,10 +3,10 @@
 namespace Sztyup\Nexus\Middleware;
 
 use Closure;
-use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Contracts\View\Factory;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Sztyup\Nexus\ImpersonationManager;
 use Sztyup\Nexus\Traits\InjectorMiddleware;
 
 class Impersonate
@@ -15,52 +15,38 @@ class Impersonate
 
     const SESSION_KEY = '_nexus_impersonate';
 
-    /** @var Guard */
-    protected $guard;
-
     /** @var Factory */
     protected $viewFactory;
 
-    public function __construct(Guard $guard, Factory $factory)
+    protected $manager;
+
+    public function __construct(Factory $factory, ImpersonationManager $impersonationManager)
     {
-        $this->guard = $guard;
         $this->viewFactory = $factory;
+        $this->manager = $impersonationManager;
     }
 
     public function handle(Request $request, Closure $next)
     {
-        if ($request->session()->has(self::SESSION_KEY)) {
-            if ($request->user() == null) {
-                // Make sure unauthenticad users cant impersonate (eg. expired oauth)
-                if ($request->session()->has(self::SESSION_KEY)) {
-                    $request->session()->remove(self::SESSION_KEY);
-                }
-            } else {
-                $this->guard->onceUsingId(
-                    $request->session()->get(self::SESSION_KEY)
-                );
+        $this->manager->handleRequest($request);
 
-                /** @var Response $response */
-                $response = $next($request);
+        /** @var Response $response */
+        $response = $next($request);
 
-                if ($this->shouldInject($response)) {
-                    $this->injectImpersonateBar($response);
-                }
-
-                return $response;
-            }
+        if ($this->manager->isImpersonating() && $this->shouldInject($response)) {
+            $this->injectImpersonateBar($response);
         }
 
-        return $next($request);
+        return $response;
     }
 
-    protected function injectImpersonateBar(Response &$response)
+    protected function injectImpersonateBar(Response $response)
     {
-        $impersonate = $this->viewFactory->make('nexus::impersonate', [
-            'user' => $this->guard->user()
+        $view = $this->viewFactory->make('nexus::impersonate', [
+            'user' => $this->manager->getImpersonatedUser()
         ])->render();
 
-        $content = preg_replace("/<body([^>])*>/", "\\0\n$impersonate", $response->getContent());
+        $content = preg_replace("/<body([^>])*>/", "\\0\n$view", $response->getContent());
 
         $response->setContent($content);
     }
