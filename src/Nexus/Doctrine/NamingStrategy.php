ebd0aad6760d3abb7ac2100535faba7188b67ea9
sztyup <sztyup@gmail.com>
2018-03-29 11:48:05 +0200

    Add doctrine naming strategy

---
+++ src/Nexus/Doctrine/NamingStrategy.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Doctrine;
+
+use Illuminate\Support\Str;
+use LaravelDoctrine\ORM\Configuration\LaravelNamingStrategy;
+
+class NexusNamingStrategy extends LaravelNamingStrategy
+{
+    public function classToTableName($className)
+    {
+        $site = $this->getSiteNameFromEntityClass($className);
+
+        if ($site) {
+            return Str::slug($site) . '_' . parent::classToTableName($className);
+        }
+
+        return parent::classToTableName($className);
+    }
+
+    private function getSiteNameFromEntityClass($className)
+    {
+        $className = is_object($className) ? get_class($className) : $className;
+
+        $parts = explode('\\', $className);
+        $ns = array_slice($parts, -2, 1)[0];
+
+        if ($ns == 'Entities') {
+            return null;
+        } else {
+            return $ns;
+        }
+    }
+}
