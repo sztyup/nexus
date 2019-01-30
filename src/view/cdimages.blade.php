341a2ab190d473bdee1ebc9357654bec9aa4ec52
sztyup <sztyup@gmail.com>
2019-01-30 18:05:27 +0100

    Fix images

--- src/view/cdimages.blade.php
+++ src/view/cdimages.blade.php
@@ -1,6 +1,6 @@
 {{-- DO NOT OVERRIDE OR EDIT THIS VIEW --}}
 <!-- Cross domain login handling -->
 @foreach($sites as $site)
-    <img style="visibility: hidden" src="{{ $site->route('auth.internal') }}?s_code={{ $code }}"/>
+    <img style="visibility: hidden; height: 0;" src="{{ $site->route('auth.internal') }}?s_code={{ $code }}"/>
 @endforeach
 <!-- End of cross domain login handling -->
